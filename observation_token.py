0003b90f3e3fae37cab7110753b00a64b5d78f52
ecal <ecallanan04@gmail.com>
2021-05-12 10:59:37 -0400

    Add documentation

--- observation_token.py
+++ observation_token.py
@@ -14,7 +14,22 @@ class token_method(Enum):
 
 
 class ObservationToken:
+    """
+    An ObservationToken object implements a `tokenize` function to generate an
+    observation token for an action-state pair.
+    """
+
     def __init__(self, method=token_method.IDENTITY):
+        """
+        Creates an ObservationToken object. This will store the supplied
+        tokenize method to use on action-state pairs.
+
+        Attributes
+        ----------
+        tokenize : function
+            The function to apply to an action-state pair to generate the
+            observation token.
+        """
         if isinstance(method, token_method):
             self.tokenize = self.get_method(method)
         elif callable(method):
@@ -25,9 +40,39 @@ class ObservationToken:
         else:
             raise InvalidMethod(method)
 
-    def get_method(self, method):
-        if method == token_method.IDENTITY:
-            return self.identity
+    def get_method(self, method) -> object:
+        """
+        Retrieves a predefined `tokenize` function.
 
-    def identity(self, action, state):
+        Arguments
+        ---------
+        method : Enum
+            The enum corresponding to the predefined `tokenize` function.
+
+        Returns
+        -------
+        tokenize : object
+            The `tokenize` function reference.
+        """
+        tokenize = lambda _: None
+        if method == token_method.IDENTITY:
+            tokenize = self.identity
+        return tokenize
+
+    def identity(self, action, state) -> tuple:
+        """
+        The identity `tokenize` function.
+
+        Arguments
+        ---------
+        action : Action
+            An `Action` object.
+        state : list
+            An list of fluents representing the state.
+
+        Returns
+        -------
+        tokenize : function
+            The `tokenize` function reference.
+        """
         return (action, state)
