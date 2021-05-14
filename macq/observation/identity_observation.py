62700e98e6ecd8c460133b9d229ab65aa4927685
ecal <ecallanan04@gmail.com>
2021-05-14 10:42:16 -0400

    Rename files to lower-case

--- macq/observation/identity_observation.py
+++ macq/observation/identity_observation.py
@@ -5,6 +5,4 @@ from .ObservationToken import Observation
 class IdentityObservation(Observation):
     def __init__(self, step: Step):
         super().__init__(step)
-        # if this wasn't identity there would be more processing before
-        # assigning self.token
         self.token = step
