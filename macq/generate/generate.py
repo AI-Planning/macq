8030e78f21cf4515cafa808d3f7a5d7caae337f3
Rebecca De Venezia <rebeccaleahdevenezia@gmail.com>
2021-05-19 17:56:56 -0400

    starting to add parsing functions

--- macq/generate/generate.py
+++ macq/generate/generate.py
@@ -1,6 +1,6 @@
 #from ..trace import TraceList, Trace
 import random
-from macq.trace import TraceList, Trace, Step, Action, State
+from macq.trace import TraceList, Trace, Step, Action, State, CustomObject
 from pathlib import Path
 import tarski
 from tarski.io import PDDLReader
@@ -9,6 +9,7 @@ from tarski.search.operations import progress, is_applicable
 from tarski.grounding import LPGroundingStrategy
 from tarski.grounding.lp_grounding import ground_problem_schemas_into_plain_operators
 from tarski.grounding.errors import ReachabilityLPUnsolvable
+from tarski.syntax.ops import CompoundFormula
 
 def generate_traces(dom, prob, plan_len : int, num_traces : int):
     """
@@ -36,22 +37,10 @@ def generate_traces(dom, prob, plan_len : int, num_traces : int):
     reader.parse_domain(dom)
     problem = reader.parse_instance(prob)
     lang = problem.language
-    #actions = [value for value in problem.actions.values()]
     grounder = tarski.grounding.LPGroundingStrategy(reader.problem)
     actions = grounder.ground_actions()
-    print(actions)
-    print()
-    print(grounder.ground_state_variables())
     operators = ground_problem_schemas_into_plain_operators(problem)
-    print()
-    print(operators)
     instance = GroundForwardSearchModel(problem, operators)
-
-    #is_applicable(problem.init,grounder.ground_state_variables()[0])
-
-    #try again with ground forward - ask why you get errors here and inspect the errors deeper
-    #(grounded operator -> action w/ no parameters? but there is no other applicable function...
-    # and then just trying to use is_applicable doesn't work....)
 
     traces = TraceList()
     for i in range(num_traces):
@@ -59,18 +48,54 @@ def generate_traces(dom, prob, plan_len : int, num_traces : int):
         state = problem.init
         for j in range(plan_len):
             app_act = instance.applicable(state)
-            print(app_act)
             ls = []
             for item in app_act:
-                #pass
                 ls.append(item)
             act = random.choice(ls)
-            trace.append(Step(Action(act), State(state)))
-            next_state = progress(state, act)
+            action = tarski_typed_act_to_macq(act)
+            #trace.append(Step(Action(act), State(state)))
+            #next_state = progress(state, act)
         traces.append(trace)
 
-#def tarski_act_to_macq(act: ):
+def tarski_typed_act_to_macq(act: tarski.fstrips.action.PlainOperator):
+    print(act)
+    split = typing_split(act.name)
 
+    pre = act.precondition
+    print(split)
+    print(pre)
+    print(split_fluents(pre))
+
+def typing_split(name: str):
+    split = {}
+    name = name.strip(')')
+    takes_param = name.split('(')[0]
+    param_names = name.split('(')[1].split(', ')
+    num_param = len(param_names)
+    obj_param = []
+    for i in range(num_param//2):
+        obj_param.append(CustomObject(param_names[i + num_param//2], param_names[i]))
+    split['takes param'] = takes_param
+    split['objects'] = obj_param
+    return split
+
+def split_fluents(fluents: CompoundFormula):
+    fluents = fluents.subformulas
+    for fluent in fluents:
+        # remove starting and ending parentheses, if necessary
+        name = str(fluent)
+        if name[0] == '(':
+            name = name.strip(')')
+            name = name[1:]
+        test =  name.split(' ')
+        print(fluent)
+        print(name)
+        print(test)
+        print()
+    return fluents
+
+def tarski_state_to_macq(state: tarski.model.Model):
+    pass
 if __name__ == "__main__":
     # exit out to the base macq folder so we can get to /tests 
     base = Path(__file__).parent.parent.parent
