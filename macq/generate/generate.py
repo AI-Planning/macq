41b55e37fca8388e75ac2e84a1dca5861cffc883
beckydvn <18rldv@queensu.ca>
2021-05-19 14:58:59 -0400

    attempting to ground

--- macq/generate/generate.py
+++ macq/generate/generate.py
@@ -1,8 +1,14 @@
 #from ..trace import TraceList, Trace
-from macq.trace import TraceList, Trace
+import random
+from macq.trace import TraceList, Trace, Step, Action, State
+from pathlib import Path
 import tarski
 from tarski.io import PDDLReader
-from pathlib import Path
+from tarski.search import GroundForwardSearchModel
+from tarski.search.operations import progress, is_applicable
+from tarski.grounding import LPGroundingStrategy
+from tarski.grounding.errors import ReachabilityLPUnsolvable
+#tarski.grounding.
 
 def generate_traces(dom, prob, plan_len : int, num_traces : int):
     """
@@ -29,49 +35,41 @@ def generate_traces(dom, prob, plan_len : int, num_traces : int):
     reader = PDDLReader(raise_on_error=True)
     reader.parse_domain(dom)
     problem = reader.parse_instance(prob)
-    actions = problem.actions
     lang = problem.language
-    precond = {}
-    for get_act in actions:
-        action = problem.get_action(get_act)
-        precond[action.name] = action.precondition
+    #actions = [value for value in problem.actions.values()]
+    grounder = tarski.grounding.LPGroundingStrategy(reader.problem)
+    actions = grounder.ground_actions()
+    print(actions)
 
-    state = problem.init
-    action1 = list(problem.actions)[0]
-    song = lang.get('classical_song')
-    test = precond[action1]
-    print(type(problem.goal))
-    print(type(test))
-    print(state[problem.goal])
-    print(state)
-    print()
-    print(problem.goal)
-    print(test)
-    #doesn't work because you need a predicate formula with "concrete" atoms
-    print(state[test])
+    instance = GroundForwardSearchModel(problem, actions)
+
+    is_applicable(problem.init, actions[0])
+
+    #try again with ground forward - ask why you get errors here and inspect the errors deeper
+    #(grounded operator -> action w/ no parameters? but there is no other applicable function...
+    # and then just trying to use is_applicable doesn't work....)
 
     traces = TraceList()
     for i in range(num_traces):
         trace = Trace()
-        # get initial state
         state = problem.init
-        
-        
         for j in range(plan_len):
-            app_act = []
-            # get applicable actions
-            # get all actions and preconditions; compare to the current state
-            for action in list(problem.actions):
-                if problem.init[precond[action]]:
-                    app_act.append(action)
-                print(app_act)
-                
+            app_act = instance.applicable(state)
+            print(app_act)
+            ls = []
+            for item in app_act:
+                #pass
+                ls.append(item)
+            act = random.choice(app_act)
+            trace.append(Step(Action(act), State(state)))
+            next_state = progress(state, act)
+        traces.append(trace)
 
-            
+#def tarski_act_to_macq(act: ):
 
-# exit out to the base macq folder so we can get to /tests 
-base = Path(__file__).parent.parent.parent
-dom = (base / 'tests/pddl_testing_files/domain.pddl').resolve()
-prob = (base / 'tests/pddl_testing_files/problem.pddl').resolve()
-generate_traces(dom, prob, 1, 1)
-
+if __name__ == "__main__":
+    # exit out to the base macq folder so we can get to /tests 
+    base = Path(__file__).parent.parent.parent
+    dom = (base / 'tests/pddl_testing_files/domain.pddl').resolve()
+    prob = (base / 'tests/pddl_testing_files/problem.pddl').resolve()
+    generate_traces(dom, prob, 1, 1)
