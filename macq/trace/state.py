a402b8aba4fc95338db712239839d84b51eb3f5c
ecal <ecallanan04@gmail.com>
2021-05-26 11:04:13 -0400

    Make State dict-like

--- macq/trace/state.py
+++ macq/trace/state.py
@@ -1,4 +1,3 @@
-from typing import List
 from . import Fluent
 
 
@@ -12,12 +11,44 @@ class State:
             A list of fluents representing the state.
     """
 
-    def __init__(self, fluents: List[Fluent]):
+    def __init__(self, fluents: dict[Fluent, bool] = {}):
         self.fluents = fluents
 
+    def __len__(self):
+        return len(self.fluents)
+
+    def __setitem__(self, key: Fluent, value: bool):
+        self.fluents[key] = value
+
+    def __getitem__(self, key: Fluent):
+        return self.fluents[key]
+
+    def __delitem__(self, key: Fluent):
+        del self.fluents[key]
+
+    def __repr__(self):
+        return repr(self.fluents)
+
     def __str__(self):
-        string = "State:\n\n"
-        for fluent in self.fluents:
-            string += str(fluent) + "\n"
-        string = string.rstrip()
-        return string
+        return str(self.fluents)
+
+    def clear(self):
+        return self.fluents.clear()
+
+    def copy(self):
+        return self.fluents.copy()
+
+    def has_key(self, k):
+        return k in self.fluents
+
+    def update(self, *args, **kwargs):
+        return self.fluents.update(*args, **kwargs)
+
+    def keys(self):
+        return self.fluents.keys()
+
+    def values(self):
+        return self.fluents.values()
+
+    def items(self):
+        return self.fluents.items()
