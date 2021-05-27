5bad4a06fbd5b94aa0fdc0d1a0a266138bdbd67e
ecal <ecallanan04@gmail.com>
2021-05-27 17:15:49 -0400

    Replace delta_states with Action updates

--- macq/extract/observer.py
+++ macq/extract/observer.py
@@ -19,33 +19,32 @@ class Observer:
 
     @staticmethod
     def _get_actions(traces: TraceList):
-        delta_states = defaultdict(DeltaState)
         for trace in traces:
             for action in trace.actions:
                 if action is not None:
                     sas_triples = trace.get_sas_triples(action)  # list
                     for sas in sas_triples:
                         delta = sas.pre_state.diff_from(sas.post_state)
-                        delta_states[action].pre_cond.update(delta.pre_cond)
-                        delta_states[action].added.update(delta.added)
-                        delta_states[action].deleted.update(delta.deleted)
+                        action.update_preconds(delta.pre_cond)
+                        action.update_add(delta.added)
+                        action.update_delete(delta.deleted)
 
-        print("Delta states:")
-        indent = " " * 2
-        for action, delta in delta_states.items():
-            print(f"{indent}Action: {action}")
-            if delta.pre_cond is not None:
-                print(f"{indent*3}pre_cond:")
-                for f in delta.pre_cond:
-                    print(f"{indent*4}{f}")
-            if delta.added is not None:
-                print(f"{indent * 3}added:")
-                for f in delta.added:
-                    print(f"{indent* 4}{f}")
-            if delta.deleted is not None:
-                print(f"{indent*3}deleted:")
-                for f in delta.deleted:
-                    print(f"{indent*4}{f}")
+            # print("Delta states:")
+            # indent = " " * 2
+            # for action, delta in delta_states.items():
+            #     print(f"{indent}Action: {action}")
+            #     if delta.pre_cond is not None:
+            #         print(f"{indent*3}pre_cond:")
+            #         for f in delta.pre_cond:
+            #             print(f"{indent*4}{f}")
+            #     if delta.added is not None:
+            #         print(f"{indent * 3}added:")
+            #         for f in delta.added:
+            #             print(f"{indent* 4}{f}")
+            #     if delta.deleted is not None:
+            #         print(f"{indent*3}deleted:")
+            #         for f in delta.deleted:
+            #             print(f"{indent*4}{f}")
 
             print()
 
