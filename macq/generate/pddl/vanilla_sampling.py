7c4d3050bd9e17dcde274ecb4f6e351c6a495f3a
beckydvn <rebeccaleahdevenezia@gmail.com>
2021-05-27 09:20:14 -0400

    remove testing/cleanup

--- macq/generate/pddl/vanilla_sampling.py
+++ macq/generate/pddl/vanilla_sampling.py
@@ -77,11 +77,3 @@ class VanillaSampling(Generate):
             if valid_trace:
                 traces.append(trace)
         return traces
-
-if __name__ == "__main__":
-    # exit out to the base macq folder so we can get to /tests 
-    base = Path(__file__).parent.parent.parent.parent
-    dom = (base / 'tests/pddl_testing_files/blocks_domain.pddl').resolve()
-    prob = (base / 'tests/pddl_testing_files/blocks_problem.pddl').resolve()
-    vanilla = VanillaSampling(dom, prob, 3, 1)
-    print(vanilla.generate_traces())
