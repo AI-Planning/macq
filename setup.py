c56556eb5e32c1287aa894d393db10b7c3965ade
ecal <ecallanan04@gmail.com>
2021-05-31 12:24:47 -0400

    Allow python 3.7

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ CLASSIFIERS = [
     "Topic :: Scientific/Engineering :: Mathematics",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3 :: Only",
@@ -46,7 +47,7 @@ setup(
     url="https://github.com/QuMuLab/macq",
     classifiers=CLASSIFIERS,
     packages=find_packages("macq/macq"),
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=DEPENDENCIES,
     extras_require={"dev": DEV_DEPENDENCIES},
 )
