383f67497fac3de38642f566f37c36014fad0bff
ecal <ecallanan04@gmail.com>
2021-06-02 17:46:52 -0400

    [WIP] Make Observer extract fluents and actions from observations

--- macq/extract/observer.py
+++ macq/extract/observer.py
@@ -1,69 +1,82 @@
 from collections import defaultdict
+from macq.extract.extract import IncompatibleObservationToken
+from typing import List
 from .model import Model
-from ..trace import TraceList, Action
+from ..trace import ObservationList, Action
+from ..observation import Observation, IdentityObservation
 
 
 class Observer:
     """Observer model extraction method.
 
-    Extracts a Model from state traces using the observer technique.  All
-    the fluents present in the traces are saved as is. The actions have their
-    preconditions, add effects, and delete effects found and added to the
+    Extracts a Model from state observations using the observer technique.  All
+    the fluents present in the observations are saved as is. The actions have
+    their preconditions, add effects, and delete effects found and added to the
     object.
 
-    The preconditions of an action are defined as the (positive)
-    intersection of all pre-states to that action. The effects are the union
-    off all fluents that changed state from a pre-state to a post-state of an
-    action. Add effects are fluents that went from False to True, delete
-    effects are fluents that went from True to False.
-
-    Assumptions:
-        - fully deterministic
-        - fully observable
-        - no noise
-        - no conditional effects
-
+    The preconditions of an action are defined as the (positive) intersection
+    of all pre-states to that action. The effects are the union off all fluents
+    that changed state from a pre-state to a post-state of an action. Add this
+    effects are fluents that went from False to True, delete effects are
+    fluents that went from True to False.
     """
 
-    def __new__(cls, traces: TraceList):
+    def __new__(cls, observations: ObservationList):
         """Creates a new Model object.
 
         Args:
-            traces (TraceList):
-                The state traces to extract the model from.
+            observations (ObservationList):
+                The state observations to extract the model from.
+        Raises:
+            IncompatibleObservationToken:
+                Raised if the observations are not identity observation.
         """
-        fluents = Observer._get_fluents(traces)
-        actions = Observer._get_actions(traces)
+        if observations.type is not IdentityObservation:
+            raise IncompatibleObservationToken(
+                "The Observer extraction technique only works with identity observations. Use `macq.observation.IdentityObservation` as the `Token` when tokenizing traces."
+            )
+        fluents = Observer._get_fluents(observations)
+        actions = Observer._get_actions(observations)
         return Model(fluents, actions)
 
     @staticmethod
-    def _get_fluents(traces: TraceList):
-        """Retrieves the set of fluents in the traces."""
+    def _get_fluents(observations: ObservationList):
+        """Retrieves the set of fluents in the observations."""
         fluents = set()
-        for trace in traces:
-            for fluent in trace.fluents:
-                fluents.add(fluent)
+        trace: List[Observation]
+        for trace in observations:
+            for obs in trace:
+                # Update fluents with the fluents in this observation
+                fluents.update(list(obs.step.state.keys()))
         return fluents
 
     @staticmethod
-    def _get_actions(traces: TraceList):
-        """Retrieves and augments the set of actions in the traces."""
+    def _get_actions(observations: ObservationList):
+        """Retrieves and augments the set of actions in the observations."""
         # Generator code already augments actions
         # If that remains the case, will need to wipe the action's attributes
         # here or earlier in Extract
-        action_pre_states = defaultdict(set)
-        for trace in traces:
-            action: Action
-            for action in trace.actions:
+
+        # Get the unique actions
+        actions: Set[Action] = set()
+        trace: List[Observation]
+        for trace in observations:
+            for obs in trace:
+                action = obs.step.action
                 if action is not None:  # Final step has no action
-                    sas_triples = trace.get_sas_triples(action)  # (S,A,S')
-                    for sas in sas_triples:
-                        # Add all action pre-states to a set
-                        action_pre_states[action].add(sas.pre_state)
-                        # Directly add effects
-                        delta = sas.pre_state.diff_from(sas.post_state)
-                        action.update_add(delta.added)
-                        action.update_delete(delta.deleted)
+                    actions.add(action)
+
+        # Create the ModelActions
+        action_pre_states = defaultdict(set)
+        for action in actions:
+            sas_triples = trace.get_sas_triples(action)  # (S,A,S')
+            for sas in sas_triples:
+                # Add all action pre-states to a set
+                action_pre_states[action].add(sas.pre_state)
+                # Directly add effects
+                delta = sas.pre_state.diff_from(sas.post_state)
+                action.update_add(delta.added)
+                action.update_delete(delta.deleted)
 
         for action, pre_states in action_pre_states.items():
             # Find the (positive) intersection of the pre-states
