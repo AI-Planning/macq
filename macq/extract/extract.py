46e7f480d64bb23c7387ae3d48a0bf6f8db0ef31
ecal <ecallanan04@gmail.com>
2021-06-02 17:46:13 -0400

    Add a custom exception to Extract for bad observations

--- macq/extract/extract.py
+++ macq/extract/extract.py
@@ -1,6 +1,14 @@
 from enum import Enum, auto
 from .observer import Observer
-from ..trace import TraceList
+from ..trace import ObservationList
+
+
+class IncompatibleObservationToken(Exception):
+    def __init__(
+        self,
+        message="The observations are not compatible with the extraction technique.",
+    ):
+        super().__init__(message)
 
 
 class modes(Enum):
@@ -13,21 +21,21 @@ class modes(Enum):
 
 
 class Extract:
-    """Extracts models from traces.
+    """Extracts models from observations.
 
     The Extract class uses an extraction method to retrieve an action Model
-    from state traces.
+    from state observations.
     """
 
-    def __new__(cls, traces: TraceList, mode: modes):
+    def __new__(cls, observations: ObservationList, mode: modes):
         """Extracts a Model object.
 
-        Extracts a model from the traces using the specified extraction
+        Extracts a model from the observations using the specified extraction
         technique.
 
         Args:
-            traces (TraceList):
-                The state traces to extract the model from.
+            observations (ObservationList):
+                The state observations to extract the model from.
             mode (Enum):
                 The extraction technique to use.
 
@@ -36,4 +44,4 @@ class Extract:
             extraction technique used.
         """
         if mode == modes.OBSERVER:
-            return Observer(traces)
+            return Observer(observations)
