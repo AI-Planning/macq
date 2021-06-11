10be17c7631f4aea509ea8288ef2ebb6231a7caa
ecal <ecallanan04@gmail.com>
2021-06-11 17:10:37 -0400

    Add holds method to State, dual purpose in operator

--- macq/trace/state.py
+++ macq/trace/state.py
@@ -52,7 +52,7 @@ class State:
         return iter(self.fluents)
 
     def __contains__(self, key):
-        return self.fluents.__contains__(key)
+        return self.fluents[key]
 
     def clear(self):
         return self.fluents.clear()
@@ -77,6 +77,11 @@ class State:
 
     def clone(self):
         return State(self.fluents)
+
+    def holds(self, fluent: str):
+        fluents = dict(map(lambda f: (f.name, f), self.keys()))
+        if fluent in fluents.keys():
+            return self[fluents[fluent]]
 
     def diff_from(self, other: State):
         """Determines the delta-state between this state and `other`.
