4ef4eaa8c0d5f8cff229691d1240348d7512c4e3
ecal <ecallanan04@gmail.com>
2021-06-15 10:18:25 -0400

    Replace objects with detail strings in IdentityObs

--- macq/observation/identity_observation.py
+++ macq/observation/identity_observation.py
@@ -17,8 +17,8 @@ class IdentityObservation(Observation):
             The step associated with this observation.
         """
         super().__init__(index=step.index, **kwargs)
-        self.state = step.state.clone()
-        self.action = None if step.action is None else step.action.clone()
+        self.state = {fluent.details(): value for fluent, value in step.state.items()}
+        self.action = None if step.action is None else step.action.details()
 
     def __hash__(self):
         return hash(self.details())
@@ -32,11 +32,11 @@ class IdentityObservation(Observation):
         if key == "action":
             if self.action is None:
                 return value is None
-            return self.action.details() == value
+            return self.action == value
         elif key == "fluent_holds":
-            return self.state.holds(value)  # whatever this needs to look like
+            return self.state[value]
         else:
             raise InvalidQueryParameter(IdentityObservation, key)
 
     def details(self):
-        return str(self.index) + self.action.details() + self.state.details()
+        return f"Obs {str(self.index)}.\n  State: {str(self.state)}\n  Action: {str(self.action)}"
