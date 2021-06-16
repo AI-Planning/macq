cfbbed07f262f5c12372fb809933a09f6adff768
beckydvn <rebeccaleahdevenezia@gmail.com>
2021-06-16 16:31:10 -0400

    clean up

--- macq/extract/slaf.py
+++ macq/extract/slaf.py
@@ -50,17 +50,12 @@ class Slaf:
 
     @staticmethod
     def get_initial_fluent_factored(observation: Observation):
-        # fluent_factored has to be a bauhaus formula. need to know if bauhaus formula can use regular Vars.
-        # If it does, revert your other branch to the old version of the token that just gets vars, and only
-        # use bauhaus stuff for the action propositions. once you figure out how to represent everything
-        # cleanly in bauhaus, make sure the formula is in the correct fluent-factored form. Finally, you can
-        # code the algorithm, starting with the fluent-factored form returned from this function. Also, note
-        # that the AS-STRIPS-SLAF algorithm loops through the action/observation pairs...
         raw_fluent_factored = []
         fluents = set()
         fluents.update(
             f for token in observation for f in token.get_base_true_fluents()
         )
+        # set up the initial fluent factored form for the problem
         for f in fluents:
             phi = {}
             phi["fluent"] = f
@@ -85,7 +80,7 @@ class Slaf:
                 a = token.step.action
                 all_o = token.step.state.children
                 # iterate through every fluent in the fluent-factored transition belief formula
-                # steps 1. (a)-(c)
+                # steps 1. (a)-(c) of AS-STRIPS-SLAF
                 for phi in raw_fluent_factored:
                     f = phi["fluent"]
                     pos_precond = ActPrecond(a, f).compile()
@@ -104,11 +99,10 @@ class Slaf:
                     phi["neg expl"] = neg_effect | (
                         neutral & ~pos_precond & phi["neg expl"]
                     )
-                    # apply the constraints to this action and all fluents (5.2, 1-3)
+                    # apply the constraints to this action and all fluents (Section 5.2, 1-3)
                     constraint.add_exactly_one(e, pos_effect, neg_effect, neutral)
                     constraint.add_at_most_one(e, pos_precond, neg_precond)
-
-                # steps 1. (d)-(e)
+                # steps 1. (d)-(e) of AS-STRIPS-SLAF
                 for phi in raw_fluent_factored:
                     f = phi["fluent"]
                     if f in all_o:
@@ -120,12 +114,13 @@ class Slaf:
                         phi["neg expl"] = f | true
                         phi["neutral"] = phi["neutral"] & phi["neg expl"]
             # convert to formula once you have stepped through the whole observation/trace and applied all transformations
-            # add as constraints to e here?
+            # add as constraints to e here
             for phi in raw_fluent_factored:
                 f = phi["fluent"]
                 # convert to formula for each fluent
                 e.add_constraint(
                     (~f | phi["pos expl"]) & (f | phi["neg expl"]) & phi["neutral"]
                 )
+        print(type(e))
         e = e.compile()
         print(e)
