e325bc10f35e3e6bdb9cadb171ac9690093a55c2
beckydvn <rebeccaleahdevenezia@gmail.com>
2021-06-16 10:08:17 -0400

    comments

--- macq/extract/slaf.py
+++ macq/extract/slaf.py
@@ -23,27 +23,6 @@ class Slaf:
             raise extract.IncompatibleObservationToken(observations.type, Slaf)
         Slaf.get_initial_fluent_factored(observations)
 
-    @staticmethod
-    def get_initial_fluent_factored(observations: ObservationList):
-        fluent_factored = None
-        for obs in observations:
-            for token in obs:
-                for f in token.step.state:
-                    if fluent_factored and f in fluent_factored.vars():
-                        continue
-                    if not f.true:
-                        f = ~f
-                    conj = (~f | true) & (f | true) & true
-                    print(conj)
-                    conj = conj.make_smooth()
-                    print(conj)
-                    if fluent_factored:
-                        fluent_factored = fluent_factored & conj
-                    else:
-                        fluent_factored = conj
-        print(fluent_factored)
-        return fluent_factored
-
     @proposition(e)
     class ActionPrecondition(object):
         def __init__(self, action: Action, fluent: Var):
@@ -61,3 +40,32 @@ class Slaf:
         def __init__(self, action: Action, fluent: Var):
             self.action = action.name
             self.fluent = fluent
+
+    @staticmethod
+    def get_initial_fluent_factored(observations: ObservationList):
+        fluent_factored = None
+        # fluent_factored has to be a bauhaus formula. need to know if bauhaus formula can use regular Vars.
+        # If it does, revert your other branch to the old version of the token that just gets vars, and only
+        # use bauhaus stuff for the action propositions. once you figure out how to represent everything
+        # cleanly in bauhaus, make sure the formula is in the correct fluent-factored form. Finally, you can
+        # code the algorithm, starting with the fluent-factored form returned from this function. Also, note
+        # that the AS-STRIPS-SLAF algorithm loops through the action/observation pairs...
+        for obs in observations:
+            for token in obs:
+                for f in token.step.state:
+                    test = Slaf.ActionPrecondition(token.step.action, f)
+                    test_form = test.fluent | f
+                    if fluent_factored and f in fluent_factored.vars():
+                        continue
+                    if not f.true:
+                        f = ~f
+                    conj = (~f | true) & (f | true) & true
+                    print(conj)
+                    conj = conj.make_smooth()
+                    print(conj)
+                    if fluent_factored:
+                        fluent_factored = fluent_factored & conj
+                    else:
+                        fluent_factored = conj
+        print(fluent_factored)
+        return fluent_factored
