89819c7f2655ce48ba7859bed5b7a6704233e791
ecal <ecallanan04@gmail.com>
2021-06-23 15:28:23 -0400

    Test vanilla sampling errors

--- tests/generate/pddl/test_vanilla_sampling.py
+++ tests/generate/pddl/test_vanilla_sampling.py
@@ -1,15 +1,17 @@
 import pytest
 from pathlib import Path
-from macq.utils import TraceSearchTimeOut
 from macq.generate.pddl import VanillaSampling
+from macq.generate import InvalidNumberOfTraces, InvalidPlanLength
 
-# not working on this branch, should work on model-observer
-# test the timer wrapper on vanilla trace generation
-# def test_timer_vanilla_wrapper():
-#     # exit out to the base macq folder so we can get to /tests
-#     base = Path(__file__).parent.parent.parent
-#     dom = (base / "pddl_testing_files/playlist_domain.pddl").resolve()
-#     prob = (base / "pddl_testing_files/playlist_problem.pddl").resolve()
 
-#     with pytest.raises(TraceSearchTimeOut):
-#         VanillaSampling(dom=dom, prob=prob, plan_len=10, num_traces=5)
+def test_invalid_vanilla_sampling():
+    # exit out to the base macq folder so we can get to /tests
+    base = Path(__file__).parent.parent.parent
+    dom = (base / "pddl_testing_files/playlist_domain.pddl").resolve()
+    prob = (base / "pddl_testing_files/playlist_problem.pddl").resolve()
+
+    with pytest.raises(InvalidPlanLength):
+        VanillaSampling(dom=dom, prob=prob, plan_len=-1, num_traces=5)
+
+    with pytest.raises(InvalidNumberOfTraces):
+        VanillaSampling(dom=dom, prob=prob, plan_len=5, num_traces=-1)
