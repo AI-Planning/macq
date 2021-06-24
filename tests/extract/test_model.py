a696de35cbb2f8db821652b163c8db505af3a141
ecal <ecallanan04@gmail.com>
2021-06-24 17:02:02 -0400

    Add testing for Model

--- tests/extract/test_model.py
+++ tests/extract/test_model.py
@@ -1,1 +1,10 @@
-# removed tests as model will be fundamentally changed
+from macq.extract import *
+from tests.utils.test_model import test_model as TestModel
+
+
+def test_model():
+    model = TestModel()
+    s = model.serialize()
+    assert s
+    d = Model.deserialize(s)
+    assert d == model
