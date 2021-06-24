9f576463b3ace27c8dcccbf19a2af8c70bbbb3e2
ecal <ecallanan04@gmail.com>
2021-06-24 15:21:10 -0400

    Use strings for paths

--- tests/generate/pddl/test_vanilla_sampling.py
+++ tests/generate/pddl/test_vanilla_sampling.py
@@ -7,8 +7,8 @@ from macq.generate import InvalidNumberOfTraces, InvalidPlanLength
 def test_invalid_vanilla_sampling():
     # exit out to the base macq folder so we can get to /tests
     base = Path(__file__).parent.parent.parent
-    dom = (base / "pddl_testing_files/playlist_domain.pddl").resolve()
-    prob = (base / "pddl_testing_files/playlist_problem.pddl").resolve()
+    dom = str((base / "pddl_testing_files/playlist_domain.pddl").resolve())
+    prob = str((base / "pddl_testing_files/playlist_problem.pddl").resolve())
 
     with pytest.raises(InvalidPlanLength):
         VanillaSampling(dom=dom, prob=prob, plan_len=-1, num_traces=5)
