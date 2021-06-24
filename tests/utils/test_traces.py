7569407a84bff23d9ef0aaa592c3ef76869b4c66
ecal <ecallanan04@gmail.com>
2021-06-24 15:18:49 -0400

    Use a seed in test_traces

--- tests/utils/test_traces.py
+++ tests/utils/test_traces.py
@@ -7,4 +7,6 @@ def blocks_world(num_traces: int):
     dom = str((base / "pddl_testing_files/playlist_domain.pddl").resolve())
     prob = str((base / "pddl_testing_files/playlist_problem.pddl").resolve())
 
-    return VanillaSampling(dom=dom, prob=prob, plan_len=5, num_traces=num_traces).traces
+    return VanillaSampling(
+        dom=dom, prob=prob, plan_len=5, num_traces=num_traces, seed=42
+    ).traces
