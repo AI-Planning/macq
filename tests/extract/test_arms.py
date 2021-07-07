3dcc1762184e4bdc3d9c4c69f315d88f20f61b40
ecal <ecallanan04@gmail.com>
2021-07-07 15:59:00 -0400

    Add arms test

--- tests/extract/test_arms.py
+++ tests/extract/test_arms.py
@@ -4,12 +4,12 @@ from tests.utils.generators import generate_blocks_traces
 
 
 if __name__ == "__main__":
-    traces = generate_blocks_traces(plan_len=2, num_traces=1)
+    traces = generate_blocks_traces(plan_len=10, num_traces=100)  # need a goal
     observations = traces.tokenize(
         PartialObservation,
         method="random",
         percent_missing=0.10,
     )
-    model = Extract(observations, modes.SLAF)
+    model = Extract(observations, modes.ARMS)
     print()
     print(model.details())
