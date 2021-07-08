11fc7ae4519c742644663b770754a4dcd9c72621
ecal <ecallanan04@gmail.com>
2021-07-08 17:31:50 -0400

    Move Model equality type-check assertion up

--- tests/extract/test_model.py
+++ tests/extract/test_model.py
@@ -4,11 +4,11 @@ from tests.utils.test_model import test_model as TestModel
 
 def test_model():
     model = TestModel()
+    assert model != "model"
     s = model.serialize("test_model.json")
     assert s
     d = Model.deserialize(s)
     assert d == model
-    assert model != "model"
 
     with open("test_model.json", "r") as f:
         d = f.read()
