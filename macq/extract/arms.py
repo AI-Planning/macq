9ec0ba4b7dfa08a1f9756f0a0ce1c47e648bfbfc
ecal <ecallanan04@gmail.com>
2021-07-10 15:39:24 -0400

    step 1

--- macq/extract/arms.py
+++ macq/extract/arms.py
@@ -1,7 +1,12 @@
-from typing import Set, List
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Set, List, Dict
+from nnf import Var, And, Or
+from pysat.examples.rc2 import RC2
+from pysat.formula import WCNF
 from . import LearnedAction, Model
 from .exceptions import IncompatibleObservationToken
-from ..observation import PartialObservation
+from ..observation import PartialObservation as Observation
 from ..trace import ObservationLists, Fluent
 
 
@@ -14,7 +19,7 @@ class ARMS:
     """
 
     def __new__(cls, obs_lists: ObservationLists):
-        if obs_lists.type is not PartialObservation:
+        if obs_lists.type is not Observation:
             raise IncompatibleObservationToken(obs_lists.type, ARMS)
 
         # assert that there is a goal
@@ -22,29 +27,47 @@ class ARMS:
         # get fluents from initial state
         fluents = ARMS._get_fluents(obs_lists)
         # call algorithm to get actions
-        actions = ARMS._get_actions(obs_lists)
+        actions = ARMS._arms(obs_lists, fluents)
         return Model(fluents, actions)
 
     @staticmethod
     def _check_goal(obs_lists: ObservationLists) -> bool:
         """Checks that there is a goal state in the ObservationLists."""
-        goal = False
-        obs_list: List[PartialObservation]
-        for obs_list in obs_lists:
-            obs = obs_list[-1]
-            # if obs.step.state
-        return goal
+        # TODO Depends on how Rebecca implements goals
+        return True
 
     @staticmethod
     def _get_fluents(obs_lists: ObservationLists) -> Set[Fluent]:
         """Retrieves the set of fluents in the observations."""
-        fluents = set()
-        obs_list: List[PartialObservation]
-        for obs_list in obs_lists:
-            pass
-
-        return fluents
+        return obs_lists.get_fluents()
 
     @staticmethod
-    def _get_actions(obs_lists: ObservationLists) -> Set[LearnedAction]:
-        pass
+    def _arms(obs_lists: ObservationLists, fluents: Set[Fluent]) -> Set[LearnedAction]:
+        connected_actions = ARMS._step1(obs_lists)  # actions = connected_actions.keys()
+        # constraints = ARMS._step2(obs_lists, connected_actions, fluents)
+
+        return set()  # WARNING temp
+
+    @staticmethod
+    def _step1(
+        obs_lists: ObservationLists,
+    ) -> Dict[LearnedAction, Dict[LearnedAction, Set]]:
+        """Substitute instantiated objects in each action instance with the object type."""
+
+        actions: List[LearnedAction] = []
+        for obs_action in obs_lists.get_actions():
+            # We don't support objects with multiple types right now, so no
+            # multiple type clauses need to be generated
+            types = {obj.obj_type for obj in obs_action.obj_params}
+            action = LearnedAction(obs_action.name, types)
+            actions.append(action)
+
+        connected_actions = {}
+        for i, a1 in enumerate(actions):
+            connected_actions[a1] = {}
+            for a2 in actions[i:]:  # includes connecting with self
+                intersection = a1.obj_params.intersection(a2.obj_params)
+                if intersection:
+                    connected_actions[a1][a2] = intersection
+
+        return connected_actions
