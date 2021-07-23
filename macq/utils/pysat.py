a7e2003dbb25ae71293a8252d98fddf1f4a68d5c
ecal <ecallanan04@gmail.com>
2021-07-23 14:03:04 -0400

    Fix pysat translator typing

--- macq/utils/pysat.py
+++ macq/utils/pysat.py
@@ -1,16 +1,24 @@
-from typing import List, Tuple, Dict, Hashable
+from typing import List, Tuple, Dict, Hashable, Union
 from pysat.formula import WCNF
-from nnf import And, Or, Var
+from nnf import And, Or, Var, NNF
+
+
+class NotCNF(Exception):
+    def __init__(self, clauses):
+        self.clauses = clauses
+        super().__init__(f"Cannot convert a non CNF formula to WCNF")
 
 
 def _encode(clauses: And[Or[Var]]) -> Tuple[List[List[int]], Dict[int, Hashable]]:
     decode = dict(enumerate(clauses.vars(), start=1))
     encode = {v: k for k, v in decode.items()}
-
+    clauses.simplify()
+    print(clauses)
     encoded = [
         [encode[var.name] if var.true else -encode[var.name] for var in clause]
         for clause in clauses
     ]
+    print(encoded)
 
     return encoded, decode
 
@@ -19,6 +27,8 @@ def to_wcnf(
     clauses: And[Or[Var]], weights: List[int]
 ) -> Tuple[WCNF, Dict[int, Hashable]]:
     """Converts a python-nnf CNF formula to a pysat WCNF."""
+    # if not clauses.is_CNF():
+    #     raise NotCNF(clauses)
     encoded, decode = _encode(clauses)
     wcnf = WCNF()
     wcnf.extend(encoded, weights)
