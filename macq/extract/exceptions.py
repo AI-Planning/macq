518a5ec1051678318984c0e2fd27315eb71b4a04
ecal <ecallanan04@gmail.com>
2021-07-26 17:26:34 -0400

    Add InconsistentConstraintWeight exception

--- macq/extract/exceptions.py
+++ macq/extract/exceptions.py
@@ -3,3 +3,10 @@ class IncompatibleObservationToken(Exception):
         if message is None:
             message = f"Observations of type {token.__name__} are not compatible with the {technique.__name__} extraction technique."
         super().__init__(message)
+
+
+class InconsistentConstraintWeights(Exception):
+    def __init__(self, constraint, weight1, weight2, message=None):
+        if message is None:
+            message = f"Tried to assign the constraint {constraint} conflicting weights ({weight1} and {weight2})"
+        super().__init__()
