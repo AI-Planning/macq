29b05d276024ce3ce62ac7d2e341d2c407f51c3c
ecal <ecallanan04@gmail.com>
2021-08-18 16:44:07 -0400

    Fix tests

--- tests/extract/test_slaf.py
+++ tests/extract/test_slaf.py
@@ -10,7 +10,6 @@ def test_slaf():
     traces = generate_blocks_traces(plan_len=2, num_traces=1)
     observations = traces.tokenize(
         AtomicPartialObservation,
-        method=AtomicPartialObservation.random_subset,
         percent_missing=0.10,
     )
     model = Extract(observations, modes.SLAF)
@@ -23,8 +22,12 @@ def test_slaf():
 if __name__ == "__main__":
     # exit out to the base macq folder so we can get to /tests
     base = Path(__file__).parent.parent
-    model_blocks_dom = str((base / "generated_testing_files/model_blocks_domain.pddl").resolve())
-    model_blocks_prob = str((base / "generated_testing_files/model_blocks_problem.pddl").resolve())
+    model_blocks_dom = str(
+        (base / "generated_testing_files/model_blocks_domain.pddl").resolve()
+    )
+    model_blocks_prob = str(
+        (base / "generated_testing_files/model_blocks_problem.pddl").resolve()
+    )
 
     traces = generate_blocks_traces(plan_len=2, num_traces=1)
     observations = traces.tokenize(
@@ -36,4 +39,6 @@ if __name__ == "__main__":
     model = Extract(observations, modes.SLAF, debug_mode=True)
     print(model.details())
 
-    model.to_pddl("model_blocks_dom", "model_blocks_prob", model_blocks_dom, model_blocks_prob)
+    model.to_pddl(
+        "model_blocks_dom", "model_blocks_prob", model_blocks_dom, model_blocks_prob
+    )
