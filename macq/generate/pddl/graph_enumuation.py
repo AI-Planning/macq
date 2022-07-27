0fe297784488a1757447034723ed5238fb7e923c
seeratparmar <72322378+seeratparmar@users.noreply.github.com>
2022-07-27 13:35:12 -0400

    Update dot

--- macq/generate/pddl/graph_enumuation.py
+++ macq/generate/pddl/graph_enumuation.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from tarski.search.operations import progress
 import networkx as nx
 import macq
+import pygraphviz
 from macq.generate.pddl import Generator
 from macq.utils import (
     set_timer_throw_exc,
@@ -51,6 +52,7 @@ class Graph_enum(Generator):
         state = self.problem.init
         G.add_node(state)
         Visited={node:False for node in G.nodes}
+        x=1
 
         Queue= [state]
         Visited[state]=True
@@ -60,8 +62,13 @@ class Graph_enum(Generator):
             for act in app_act:
                 next_state = progress(cur_node, act)
                 if next_state not in list(G.nodes): 
-                    G.add_node(next_state)
-                    Visited[next_state]=False
+                    if x <=20:
+                        #print(x)
+                        G.add_node(next_state)
+                        Visited[next_state]=False
+                        x=x+1
+                    else:
+                        return G
                 G.add_edge(cur_node,next_state, action= act)
             for node in G.neighbors(cur_node):
                 if (Visited[node]==False):
@@ -71,6 +78,7 @@ class Graph_enum(Generator):
             
 
 DG= Graph_enum(prob='C:/Users/User/tarski/docs/notebooks/benchmarks/probBLOCKS-4-2.pddl', dom='C:/Users/User/tarski/docs/notebooks/benchmarks/blocksworld.pddl',num_traces=1).traces
+'''
 plt.figure(figsize=(50,50))
 pos = nx.spring_layout(DG)
 nx.draw(DG,pos)
@@ -79,5 +87,10 @@ edge_labels = dict([((n1, n2), d['action'])
                     for n1, n2, d in DG.edges(data=True)])
 
 nx.draw_networkx_edge_labels(DG,pos,edge_labels=edge_labels,font_size=5, font_weight='bold')
+'''
+A = nx.nx_agraph.to_agraph(DG)
+A.write("k1.dot")  # write to dot file
+X3 = nx.nx_agraph.read_dot("k1.dot")  # read from dotfile
 
-plt.show()
+# You can also create .png directly with the AGraph.draw method
+#A.draw("k1.png", prog="neato")
