b08346abfb2351c8b5007e19e1f7ba7c5a75e536
seeratparmar <72322378+seeratparmar@users.noreply.github.com>
2022-08-04 16:10:07 -0400

    Update graph_enumeration

--- macq/generate/pddl/graph_enumeration.py
+++ macq/generate/pddl/graph_enumeration.py
@@ -5,6 +5,21 @@ import macq
 import pygraphviz
 from macq.generate.pddl import Generator
 import math
+from macq.trace import (
+    State,
+    Trace,
+    Action,
+)
+
+class Step:
+    
+    def __init__(self, state: State, action: Action, next_state: State, index: int):
+        
+        self.state = state
+        self.action = action
+        self.index = index
+        self.next_state=next_state
+
 
 class Graph_enum(Generator):
     
@@ -21,14 +36,44 @@ class Graph_enum(Generator):
             prob=prob,
             problem_id=problem_id,
         )
-        
+
         self.num_nodes = num_nodes
         if num_nodes > 0:
-            self.traces = self.generate_graph()
+            self.traces = self.generate_traces()
         else:
             self.traces = None
+
+
+
+    def generate_traces(self):
+        traces = self.generate_single_trace()
+        self.traces=traces
+        return traces
+
+        
+        
         #self.traces = self.generate_graph()
         
+    def generate_single_trace(self):
+        
+        trace = Trace()
+        graph= self.generate_graph()
+
+        edge_labels = dict([((n1, n2), d['label'])
+                    for n1, n2, d in graph.edges(data=True)])
+
+        DFS=list(nx.edge_dfs(graph,list(graph.nodes)[0]))
+        count=1
+        for i in DFS:
+            macq_action = self.tarski_act_to_macq(edge_labels.get(i))
+            macq_cur_state = self.tarski_state_to_macq(i[0])
+            macq_next_state = self.tarski_state_to_macq(i[1])
+            step = Step(macq_cur_state, macq_action,macq_next_state, count)
+            count+=1
+            trace.append(step)
+            
+        return trace
+
 
        
     def generate_graph(self):
@@ -49,7 +94,7 @@ class Graph_enum(Generator):
                         #print(x)
                         G.add_node(next_state)
                         Visited[next_state]=False
-                        self.num_nodes= self.num_nodes-1
+                        self.num_nodes-= 1
                     else:
                         return G
                 G.add_edge(cur_node,next_state, label= act)
@@ -61,8 +106,8 @@ class Graph_enum(Generator):
             
 
 DG= Graph_enum(prob='C:/Users/User/tarski/docs/notebooks/benchmarks/probBLOCKS-4-2.pddl', dom='C:/Users/User/tarski/docs/notebooks/benchmarks/blocksworld.pddl').traces
-
-plt.figure(figsize=(50,50))
+print(DG)
+'''plt.figure(figsize=(50,50))
 pos = nx.spring_layout(DG)
 nx.draw(DG,pos)
 
@@ -77,3 +122,4 @@ A.write("k1.dot")  # write to dot file
 X3 = nx.nx_agraph.read_dot("k1.dot")  # read from dotfile
 
 #A.draw("k1.png", prog="neato")
+'''
