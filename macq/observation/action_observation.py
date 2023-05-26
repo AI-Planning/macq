63c9cad54aa74642e1a1f97a6eeb094a521a025c
ecal <ethan@ecal.dev>
2023-05-26 13:21:44 -0400

    Fix action_observation docstring

--- macq/observation/action_observation.py
+++ macq/observation/action_observation.py
@@ -11,25 +11,18 @@ class ActionObservation(Observation):
     def __init__(
         self,
         step: Step,
+        **kwargs,
     ):
         """
         Creates an ActionObservation object, storing the step.
         Args:
             step (Step):
                 The step associated with this observation.
-            percent_missing (float):
-                The percentage of fluents to randomly hide in the observation.
-            hide (Set[Fluent]):
-                The set of fluents to explicitly hide in the observation.
         """
 
         Observation.__init__(self, index=step.index)
 
-        # If percent_missing == 1 -> self.state = None (below).
-        # This allows ARMS (and other algorithms) to skip steps when there is no
-        # state information available without having to check every mapping in
-        # the state (slow in large domains).
-        self.state = None
+        self.state = None # stateless representation
         self.action = None if step.action is None else step.action.clone()
 
     def __eq__(self, other):
