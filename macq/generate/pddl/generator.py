bc7e0de9e892c03205521d856dc3c12503f2569f
omereliy <104314623+omereliy@users.noreply.github.com>
2024-02-01 10:34:14 +0200

    Update generator.py

--- macq/generate/pddl/generator.py
+++ macq/generate/pddl/generator.py
@@ -1,5 +1,10 @@
+import re
+import time
 from time import sleep
+from pprint import pprint
 from typing import Set, List, Union
+
+from requests import Response
 from tarski.io import PDDLReader
 from tarski.search import GroundForwardSearchModel
 from tarski.search.operations import progress
@@ -405,14 +410,23 @@ class Generator:
                     if delays:
                         sleep(delays[0])
                         try:
-                            resp = requests.post(
-                                "http://solver.planning.domains/solve",
-                                verify=False,
-                                json=data,
-                            ).json()
-                            return [act["name"] for act in resp["result"]["plan"]]
+                            service_url = "https://solver.planning.domains:5001/package/lama-first/solve"
+                            solve_request = requests.post(service_url, json=data, headers=headers).json()
+                            celery_result = requests.get("https://solver.planning.domains:5001/" +
+                                                         solve_request['result'])
+                            print('Computing...')
+                            while celery_result.json().get("status", "") == 'PENDING':
+                                sleep(delays[0])
+                                celery_result = requests.get("https://solver.planning.domains:5001/" +
+                                                             solve_request['result'])
+                            sas_plan = celery_result.json()['result']['output']['sas_plan']
+                            actions_with_objects = re.findall(r'\((.*?)\)', sas_plan)
+
+                            plan_list = [f'({action})' for action in actions_with_objects]
+                            return plan_list
+
                         except TypeError:
-                            return get_api_response(delays[1:])
+                            return get_api_response2(delays[1:])
 
                 plan = get_api_response([0, 1, 3, 5, 10])
                 if plan is None:
